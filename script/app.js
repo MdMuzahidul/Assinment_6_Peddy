458b1dee48e8359c0fa64a48fed56fa657728df0
muzahid <bsmrstu19cse063@gmail.com>
2024-10-10 00:32:09 +0600

    try to add js

---
+++ script/app.js
@@ -0,0 +1,12 @@
+function loadData() {
+  fetch("https://openapi.programming-hero.com/api/peddy/pets")
+    .then((res) => res.json())
+    .then((data) => display(data));
+}
+loadData();
+function display(animals) {
+  //   const petDosplay = document.getElementById("pets-display");
+  for (const animal in animals) {
+    console.log(animal.);
+  }
+}
